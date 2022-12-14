58b2c53ca01c478541b28a78a211ec04462763b1
Serhii Vorotnoi <svorotnoy@gmail.com>
2022-12-14 21:32:49 +0200

    fix bag

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,17 +1,17 @@
-import { Profile } from '../profile/Profile';
-import { Statistics } from '../statistics/Statistics';
-import { FriendList } from '../friend-list/FriendList';
-import { TransactionHistory } from '../transaction-history/TransactionHistory';
-import user from '../profile/user.json';
-import data from '../statistics/data.json';
-import friends from '../friend-list/friends.json';
-import transactions from '../transaction-history/transactions.json';
+import { Profile } from '../Profile/Profile';
+import { Statistics } from '../Statistics/Statistics';
+import { FriendList } from '../Friend-list/FriendList';
+import { TransactionHistory } from '../Transaction-history/TransactionHistory';
+import user from '../Profile/user.json';
+import data from '../Statistics/data.json';
+import friends from '../Friend-list/friends.json';
+import transactions from '../Transaction-history/transactions.json';
 import { Container } from './App.styled';
 
 export const App = () => {
   return (
     <Container>
-    {/*-------- 1 - Social network profile --------*/}
+      {/*-------- 1 - Social network profile --------*/}
       <Profile
         username={user.username}
         tag={user.tag}
@@ -19,12 +19,12 @@ export const App = () => {
         avatar={user.avatar}
         stats={user.stats}
       />
-    {/*-------- 2 - Statistics section -------------*/}
+      {/*-------- 2 - Statistics section -------------*/}
       <Statistics title="Upload stats" stats={data} />
       {/* <Statistics stats={data} /> */}
-    {/*-------- 3 - List of friends ----------------*/}
+      {/*-------- 3 - List of friends ----------------*/}
       <FriendList friends={friends} />
-    {/*-------- 4 - History of transactions --------*/}
+      {/*-------- 4 - History of transactions --------*/}
       <TransactionHistory items={transactions} />
     </Container>
   );
