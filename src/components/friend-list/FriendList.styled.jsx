6c2be64a954eec42280fec8267f4b57ccbd79b13
Serhii Vorotnoi <svorotnoy@gmail.com>
2022-12-14 18:14:45 +0200

    01-03

--- src/components/friend-list/FriendList.styled.jsx
+++ src/components/friend-list/FriendList.styled.jsx
@@ -1,23 +1,22 @@
 import styled from '@emotion/styled';
-
 export const ListOfFriends = styled.ul``;
 
 export const Item = styled.li`
   display: flex;
   align-items: center;
-  margin-bottom: 12px;
-  padding: 14px 0;
-  width: 274px;
+  margin-bottom: 10px;
+  padding: 10px 0;
+  width: 250px;
   background-color: #fff;
   border-radius: 4px;
-  box-shadow: 1px 3px 3px #00002030, -1px 3px 3px #00003030;
+  box-shadow: 3px 3px 5px #00002030;
 `;
 
 export const Status = styled.span`
-  margin: 0 12px;
-  width: 16px;
-  height: 16px;
-  background-color: ${props => (props.isOnline ? '#51ae50' : '#ff524d')};
+  margin: 0 20px;
+  width: 15px;
+  height: 15px;
+  background-color: ${props => (props.isOnline ? '#11f50f' : '#f5100a')};
   border-radius: 50%;
 `;
 
@@ -26,7 +25,7 @@ export const Avatar = styled.img`
 `;
 
 export const Name = styled.p`
-  margin: 0 12px;
-  font-size: 24px;
-  font-weight: 500;
+  margin: 0 35px;
+  font-size: 20px;
+  font-weight: 600;
 `;
